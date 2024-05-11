f1b81f25f6363c388a7f45a9092f3b74d7aec2b5
KanezakiHiroto <kanezaki0226@gmail.com>
2024-05-11 11:55:44 +0900

    制御構文_解答

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,5 +1,15 @@
+import math
 from math import sin
 # --example--
 # print(sin(0))
 # >>> 0
 # -----------
+
+def tra_int(f, a, b, N):
+    h = (b - a) / N
+    S = 0
+    for k in range(1, N+1):
+        S += h/2 * (f(a+(k-1)*h) + f(a+k*h))
+    return S
+
+print(tra_int(sin, 0, math.pi/2, 100))
