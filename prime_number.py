7f0386aae525a23e79d91314ee23aa355114c8de
KanezakiHiroto <kanezaki0226@gmail.com>
2024-05-12 20:32:23 +0900

    関数_解答

--- prime_number.py
+++ prime_number.py
@@ -1,4 +1,37 @@
-a = input("aの値を入力: ")
-b = input("bの値を入力: ")
+def input_value(prompt):
+    while True:
+        try:
+            value = int(input(prompt))
+            if value <= 0:
+                print("自然数を入力してください")
+            else:
+                return value
+        except ValueError:
+            print("自然数を入力してください")
 
-# TODO
+a = input_value("aの値を入力: ")
+b = input_value("bの値を入力: ")
+
+def prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+if prime(a):
+    print(f"a = {a} は素数である")
+else:
+    print(f"a = {a} は素数ではない")
+
+if prime(b):
+    print(f"b = {b} は素数である")
+else:
+    print(f"b = {b} は素数ではない")
